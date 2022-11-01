84d628ae5ca0773be0cd564f14a62591eeb79ff9
Christian Andre Vilcapoma Pareja <vilcapomaparejaca1998@gmail.com>
2022-11-01 13:26:42 -0500

    probando

--- database/migrations/2022_10_31_155628_create_detalles_table.php
+++ database/migrations/2022_10_31_155628_create_detalles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('detalles', function (Blueprint $table) {
+        Schema::create('qrv_userdetalles', function (Blueprint $table) {
             $table->id();
             $table->string('v_nombres');
             $table->string('v_apellidos');
