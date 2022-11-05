4107b364e74327e17f3520ff9d5b406bb0616ad8
Yaquelin Jimenez <gemmay26@gmail.com>
2022-11-05 10:45:02 -0500

    sin cambios

--- resources/views/user/form.blade.php
+++ resources/views/user/form.blade.php
@@ -1,6 +1,6 @@
 <div class="box box-info padding-1">
     <div class="box-body">
-        
+
         <div class="form-group">
             {{ Form::label('Nombre') }}
             {{ Form::text('name', $user->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
