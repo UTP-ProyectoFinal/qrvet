dc4c2d4b984f752a6a44f5daa4a9793e3966f0b0
Yaquelin Jimenez <gemmay26@gmail.com>
2022-11-26 01:33:53 -0500

    doble wr en show

--- resources/views/pacientes/show.blade.php
+++ resources/views/pacientes/show.blade.php
@@ -52,6 +52,8 @@
                         QR de identificación del paciente<br/>
                             {!!QrCode::size(150)->generate( env('APP_URL', 'http://localhost/qrvet/public/').'validarqr/'.$paciente->v_identifica) !!}
                             <br><br>
+                            {!!QrCode::size(70)->generate( env('APP_URL', 'http://localhost/qrvet/public/').'validarqr/'.$paciente->v_identifica) !!}
+                            <br><br>
                             <input type="button" value="Imprimir" class="printbutton btn btn-info">
                         </div>
 
