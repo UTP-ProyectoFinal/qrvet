2156ad885d2a10803e32fbf4021c6d2fab8bd94b
Christian Andre Vilcapoma Pareja <vilcapomaparejaca1998@gmail.com>
2022-12-17 08:50:34 -0500

    Final embebido

--- resources/views/Nosotros.blade.php
+++ resources/views/Nosotros.blade.php
@@ -9,6 +9,7 @@
 @section('content')
     <p>Nuestro Flujo de Procesos.</p>
     <iframe width="768" height="432" src="https://miro.com/app/live-embed/uXjVPbhKo9Q=/?moveToViewport=-399,-623,1257,1270&embedId=679326476438" frameborder="0" scrolling="no" allowfullscreen></iframe>
+        <iframe title="Sales & Returns Sample v201912" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=cdad6276-f4ff-4be5-95cd-54735ba30fc7&autoAuth=true&ctid=c4a66c34-2bb7-451f-8be1-b2c26a430158" frameborder="0" allowFullScreen="true"></iframe>
 @stop
 
 @section('css')
