0b2d87410a3e9ba7cac74dfafba6ae526ac9fe9b
Christian Andre Vilcapoma Pareja <vilcapomaparejaca1998@gmail.com>
2022-12-17 06:45:37 -0500

    Agregando microservicio de Foro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -243,6 +243,7 @@
                 <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
             @else
                 <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Iniciar sesión</a>
+                <a href="http://localhost:8001" class="text-sm text-gray-700 dark:text-gray-500 underline">FAQ-Foro Clientes</a>
             @endauth
         </div>
     @endif
